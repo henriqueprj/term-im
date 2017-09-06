73e1922a389dc1576b7cdd9db8bf00aac98ec735
Henrique Ribeiro <henriqueprj@MacBook-Pro-de-Henrique.local>
2017-09-05 20:16:02 -0400

    Initial commit

---
+++ src/TermIM.Consumer/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace TermIM.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintWelcome();
+            var username = ReadUsername();
+            if (!IsUsernameValid(username))
+            {
+                Console.WriteLine("Username inválido!");
+                return;
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("Você entrou na sala 'Desenvolvimento'.");
+            Console.WriteLine();
+            
+            var factory = new ConnectionFactory { HostName = "10.0.16.31", VirtualHost = "lab", UserName = "lab", Password = "bal" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                // Garante que o Exchange exista
+				channel.ExchangeDeclare(
+                    exchange: "chat", 
+                    type: ExchangeType.Topic);
+
+                var groupName = "desenvolvedores";
+                var routingKey = $"chat.group.{groupName}";
+
+                // Cria uma nova fila não durável, auto-delete, com nome randômico
+                var queueName = channel.QueueDeclare().QueueName;
+
+                // faz o Binding Entre o Exchange e a Fila
+                channel.QueueBind(queueName, "chat", routingKey);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (sender, e) => {
+
+                    var body = e.Body;
+                    var jsonMessage = Encoding.UTF8.GetString(body);
+                    var message = JsonConvert.DeserializeObject<TermIM.Contracts.Message>(jsonMessage);
+
+                    Console.WriteLine($"{message.Sender} ({message.Timestamp.ToLocalTime():dd/MM/yyyy HH:mm:ss}):");
+                    Console.WriteLine(message.Content);
+                    Console.WriteLine();
+                };
+
+                channel.BasicConsume(
+                    queue: queueName,
+                    autoAck: true,
+                    consumer: consumer
+                );
+                
+                
+                Console.WriteLine("Pressione qualquer tecla para sair");
+                Console.ReadKey();
+            }
+        }
+        
+        private static void PrintWelcome()
+        {
+            Console.WriteLine("Bem-vindo ao Leitor TermIM (v1.0)");
+            Console.WriteLine();
+        }
+
+        private static string ReadUsername()
+        {
+            Console.Write("Informe seu username: ");
+            return Console.ReadLine();
+        }
+
+        private static bool IsUsernameValid(string username)
+        {
+            var isMatch = Regex.IsMatch(username, "^[a-zA-Z]+\\w*$");
+            return !string.IsNullOrWhiteSpace(username) || isMatch;
+        }
+    }
+}
---
+++ src/TermIM.Producer/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using RabbitMQ.Client;
+
+namespace TermIM.Producer
+{
+    class Program
+    {
+        private static readonly JsonSerializerSettings CamelCaseSerializerSettings =
+            new JsonSerializerSettings {ContractResolver = new CamelCasePropertyNamesContractResolver()};
+        
+        static void Main(string[] args)
+        {
+            PrintWelcome();
+            
+            var username = ReadUsername();
+            
+            if (!IsUsernameValid(username))
+            {
+                Console.WriteLine("Username inválido!");
+                return;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Você entrou na sala 'Desenvolvimento'.");
+            Console.WriteLine();
+
+            var factory = new ConnectionFactory
+            {
+                HostName = "10.0.16.31", 
+                VirtualHost = "lab", 
+                UserName = "lab", 
+                Password = "bal"
+            };
+            
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "chat", type: ExchangeType.Topic);
+
+                var groupName = "desenvolvedores";
+                var routingKey = $"chat.group.{groupName}";
+
+                string content;
+
+                Console.WriteLine();
+                Console.Write($"{username}: ");
+                while ((content = Console.ReadLine()) != ":q!")
+                {
+					var msg = CreateMessage(username, content);
+					var body = System.Text.Encoding.UTF8.GetBytes(msg);
+
+                    var props = channel.CreateBasicProperties();
+                    props.ContentType = "application/json";
+                    props.DeliveryMode = 2; /* persistente */
+                    
+					channel.BasicPublish(
+						exchange: "chat",
+						routingKey: routingKey,
+						basicProperties: props,
+						body: body
+					);
+
+                    Console.WriteLine();
+                    Console.Write($"{username}: ");
+                }
+
+                Console.WriteLine("Goodbye!");
+            }
+
+
+        }
+
+        private static void PrintWelcome()
+        {
+            Console.WriteLine("Bem-vindo ao TermIM (v1.0)");
+            Console.WriteLine();
+        }
+
+        private static string ReadUsername()
+        {
+            Console.Write("Informe seu username: ");
+            return Console.ReadLine();
+        }
+
+        private static bool IsUsernameValid(string username)
+        {
+            return !string.IsNullOrWhiteSpace(username) || Regex.IsMatch(username, "^[a-zA-Z]+\\w*$");
+        }
+
+        private static string CreateMessage(string sender, string message)
+        {
+            var msg = new TermIM.Contracts.Message { Timestamp = DateTime.UtcNow, Sender = sender, Content = message };
+            var json = JsonConvert.SerializeObject(msg, CamelCaseSerializerSettings);
+            return json;
+
+            //return $"{{\"timestamp\":\"{DateTime.UtcNow:O}\",\"sender\":\"{sender}\",\"message\":\"{message}\"}}";
+        }
+    }
+}
